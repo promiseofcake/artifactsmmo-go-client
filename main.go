2df8bf26b89960c3911961d8911a819a9397c374
Lucas Kacher <lucas.kacher@chime.com>
2024-07-31 19:37:36 -0700

    feat: setup more actions, setup response schemas

--- main.go
+++ main.go
@@ -4,41 +4,44 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"time"
 
-	"github.com/promiseofcake/artifactsmmo-cli/client"
 	"github.com/promiseofcake/artifactsmmo-cli/internal/actions"
 )
 
 func main() {
 	token := os.Getenv("MMO_TOKEN")
 	if token == "" {
-		log.Fatal("no token")
+		log.Fatal("no token provided")
 	}
-
-	c, err := client.NewClientWithResponses("https://api.artifactsmmo.com", client.WithRequestEditorFn(func(ctx context.Context, req *http.Request) error {
-		req.Header.Set("Authorization", "Bearer "+token)
-		return nil
-	}))
+	runner, err := actions.NewDefaultRunner(token)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	runner := &actions.Runner{
-		Client: c,
+	// setup character
+	character := os.Getenv("MMO_CHARACTER")
+	if character == "" {
+		log.Fatal("no character")
 	}
 
+	// gather loop
 	ctx := context.Background()
 	for {
 		fmt.Println("about to gather")
-		resp, gErr := runner.Gather(ctx)
-		if gErr != nil {
+		resp, err := runner.Gather(ctx, character)
+		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("gather result status: %s (%d), cooldown: %d\n", resp.StatusText, resp.StatusCode, resp.Cooldown)
-		time.Sleep(time.Duration(resp.Cooldown) * time.Second)
-	}
 
+		sec := resp.GetRemainingCooldown()
+		fmt.Printf("gather result status: (%d), xp: %d, items: %v, cooldown: %d \n",
+			resp.StatusCode,
+			resp.SkillInfo.Xp,
+			resp.SkillInfo.Items,
+			sec,
+		)
+		time.Sleep(time.Duration(sec) * time.Second)
+	}
 }
