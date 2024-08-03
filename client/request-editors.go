1c22b3b4432e57518bc5ff93f95bc096811c3c60
Nicholas Chitty <nicholas.chitty@gmail.com>
2024-08-03 09:29:04 -0400

    Return closure instead to allow for user defined value

--- client/request-editors.go
+++ client/request-editors.go
@@ -3,10 +3,8 @@ package client
 import (
 	"context"
 	"encoding/base64"
-	"errors"
 	"fmt"
 	"net/http"
-	"os"
 )
 
 const (
@@ -15,28 +13,19 @@ const (
 	PasswordUnsetError = "ARTIFACTSMMO_PASSWORD is unset"
 )
 
-// Adds the header 'Authorization' with the value: Bearer $ARTIFACTSMMO_TOKEN to the request.
-// Returns an error if the "ARTIFACTSMMO_TOKEN" environment variable is unset.
-func AddAuthorizationTokenRequestEditor(ctx context.Context, req *http.Request) error {
-	if token, isSet := os.LookupEnv("ARTIFACTSMMO_TOKEN"); isSet {
+// Returns a request editor that adds the authorization bearer token and does not return an error.
+func NewAuthorizationBearerRequestEditor(token string) RequestEditorFn {
+	return func(ctx context.Context, req *http.Request) error {
 		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
 		return nil
 	}
-	return errors.New(TokenUnsetError)
 }
 
-// Adds the header 'Authorization' with the value: Basic base64Encode($ARTIFACTSMMO_USERNAME:$ARTIFACTSMMO_PASSWORD) to the request.
-// Returns an error if either environment variable is unset.
-func AddBasicAuthorizationRequestEditor(ctx context.Context, req *http.Request) error {
-	user, isSet := os.LookupEnv("ARTIFACTSMMO_USERNAME")
-	if !isSet {
-		return errors.New(UsernameUnsetError)
+// Returns a request editor that adds the basic authorization header and does not return an error.
+func NewBasicAuthorizationRequestEditor(username string, password string, joiner rune, encoding *base64.Encoding) RequestEditorFn {
+	return func(ctx context.Context, req *http.Request) error {
+		base64Encode := encoding.EncodeToString([]byte(username + string(joiner) + password))
+		req.Header.Add("Authorization", fmt.Sprintf("Basic %s", base64Encode))
+		return nil
 	}
-	password, isSet := os.LookupEnv("ARTIFACTSMMO_PASSWORD")
-	if !isSet {
-		return errors.New(PasswordUnsetError)
-	}
-	base64Encode := base64.StdEncoding.EncodeToString([]byte(user + ":" + password))
-	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", base64Encode))
-	return nil
 }
--- client/request-editors_test.go
+++ client/request-editors_test.go
@@ -5,24 +5,22 @@ import (
 	"encoding/base64"
 	"fmt"
 	"net/http"
-	"os"
 	"testing"
 
 	"github.com/promiseofcake/artifactsmmo-go-client/client"
 )
 
-func TestAddAuthorizationTokenRequestEditor_TokenSet(t *testing.T) {
+func TestAddAuthorizationTokenRequestEditor(t *testing.T) {
     // Given
     token := "mock_token_value"
-    os.Setenv("ARTIFACTSMMO_TOKEN", token)
-    defer os.Unsetenv("ARTIFACTSMMO_TOKEN")
 
     req := &http.Request{
         Header: make(http.Header),
     }
 
     // When
-    err := client.AddAuthorizationTokenRequestEditor(context.Background(), req)
+    requestEditor := client.NewAuthorizationBearerRequestEditor(token)
+    err := requestEditor(context.Background(), req)
 
     // Then
     if err != nil {
@@ -36,108 +34,20 @@ func TestAddAuthorizationTokenRequestEditor_TokenSet(t *testing.T) {
     }
 }
 
-func TestAddAuthorizationTokenRequestEditor_TokenUnset(t *testing.T) {
-    // Given
-    os.Unsetenv("ARTIFACTSMMO_TOKEN")
-
-    req := &http.Request{
-        Header: make(http.Header),
-    }
-
-    // When
-    err := client.AddAuthorizationTokenRequestEditor(context.Background(), req)
-
-    // Then
-    if err == nil {
-        t.Fatal("expected error, got nil")
-    }
-    if err.Error() != client.TokenUnsetError {
-        t.Fatalf("expected error message %q, got %q", client.TokenUnsetError, err.Error())
-    }
-}
-
-func TestAddBasicAuthorizationRequestEditor_UsernameUnsetPasswordUnset(t *testing.T) {
-    // Given
-    os.Unsetenv("ARTIFACTSMMO_USERNAME")
-    os.Unsetenv("ARTIFACTSMMO_PASSWORD")
-
-    req := &http.Request{
-        Header: make(http.Header),
-    }
-
-    // When
-    err := client.AddBasicAuthorizationRequestEditor(context.Background(), req)
-
-    // Then
-    if err == nil {
-        t.Fatal("expected error, got nil")
-    }
-    if err.Error() != client.UsernameUnsetError {
-        t.Fatalf("expected error message %q, got %q", client.UsernameUnsetError, err.Error())
-    }
-}
-
-func TestAddBasicAuthorizationRequestEditor_UsernameSetPasswordUnset(t *testing.T) {
-    // Given
-    username := "mock_username_value"
-    os.Setenv("ARTIFACTSMMO_USERNAME", username)
-    os.Unsetenv("ARTIFACTSMMO_PASSWORD")
-    defer os.Unsetenv("ARTIFACTSMMO_USERNAME")
-
-    req := &http.Request{
-        Header: make(http.Header),
-    }
-
-    // When
-    err := client.AddBasicAuthorizationRequestEditor(context.Background(), req)
-
-    // Then
-    if err == nil {
-        t.Fatal("expected error, got nil")
-    }
-    if err.Error() != client.PasswordUnsetError {
-        t.Fatalf("expected error message %q, got %q", client.PasswordUnsetError, err.Error())
-    }
-}
-
-func TestAddBasicAuthorizationRequestEditor_UsernameUnsetPasswordSet(t *testing.T) {
-    // Given
-    password := "mock_password_value"
-    os.Setenv("ARTIFACTSMMO_PASSWORD", password)
-    os.Unsetenv("ARTIFACTSMMO_USERNAME")
-    defer os.Unsetenv("ARTIFACTSMMO_PASSWORD")
-
-    req := &http.Request{
-        Header: make(http.Header),
-    }
-
-    // When
-    err := client.AddBasicAuthorizationRequestEditor(context.Background(), req)
-
-    // Then
-    if err == nil {
-        t.Fatal("expected error, got nil")
-    }
-    if err.Error() != client.UsernameUnsetError {
-        t.Fatalf("expected error message %q, got %q", client.UsernameUnsetError, err.Error())
-    }
-}
 
 func TestAddBasicAuthorizationRequestEditor(t *testing.T) {
     // Given
     username := "mock_username_value"
     password := "mock_password_value"
-    os.Setenv("ARTIFACTSMMO_USERNAME", username)
-    os.Setenv("ARTIFACTSMMO_PASSWORD", password)
-    defer os.Unsetenv("ARTIFACTSMMO_USERNAME")
-    defer os.Unsetenv("ARTIFACTSMMO_PASSWORD")
+    join := ':'
 
     req := &http.Request{
         Header: make(http.Header),
     }
 
     // When
-    err := client.AddBasicAuthorizationRequestEditor(context.Background(), req)
+    requestEditor := client.NewBasicAuthorizationRequestEditor(username, password, join, base64.StdEncoding)
+    err := requestEditor(context.Background(), req)
 
     // Then
     if err != nil {
@@ -145,7 +55,7 @@ func TestAddBasicAuthorizationRequestEditor(t *testing.T) {
     }
 
 
-    base64Encode := base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
+    base64Encode := base64.StdEncoding.EncodeToString([]byte(username + string(join) + password))
     expectedHeader := fmt.Sprintf("Basic %s", base64Encode)
     authHeader := req.Header.Get("Authorization")
     if authHeader != expectedHeader {
